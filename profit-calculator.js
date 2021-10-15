b6ffd69916be16b2c00980d2f6a8a0d616e1ea81
TitoxDboss <49274627+TitoxDboss@users.noreply.github.com>
2021-10-15 16:57:06 +0100

    Update profit-calculator.js

--- profit-calculator.js
+++ profit-calculator.js
@@ -1,18 +1,20 @@
 const cp = parseFloat(prompt('Enter cost price '));
 const sp= parseFloat(prompt('Enter selling price'));
-
+var  expenses = parseFloat(prompt('Enter your expenses'));
 const profit_or_loss  = ((sp-cp)/cp)*100
 
 
 if(profit_or_loss > 0){
-console.log(" Your profit is "+profit_or_loss+"%");
+console.log(" Your gross profit is "+Math.abs(profit_or_loss - expenses)+"%");
 }
 
 if(profit_or_loss < 0){
-console.log(" Your loss is "+profit_or_loss*-1+"%");
+console.log(" Your gross  loss is "+Math.abs((profit_or_loss*-1) - expenses)+"%");
 }
 
 
 if(profit_or_loss == 0){
 console.log(" You made no profit or loss");
 }
+
+
